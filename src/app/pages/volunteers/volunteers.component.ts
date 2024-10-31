c1fc0964b96ac8a304d81cf8b24369ea5769f479
Adeeb Zaman <zamanadeeb789@gmail.com>
2024-10-31 18:14:15 -0400

    Create sidenav, new pages, and bring over content

---
+++ src/app/pages/volunteers/volunteers.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-volunteers',
+  standalone: true,
+  templateUrl: './volunteers.component.html',
+  styleUrls: ['./volunteers.component.scss']
+})
+export class VolunteersComponent {}
