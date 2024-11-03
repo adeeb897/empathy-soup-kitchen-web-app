9052a9224c385aabc535d2debfe49cb5274ea0b8
Adeeb Zaman <zamanadeeb789@gmail.com>
2024-11-03 12:09:01 -0500

    Rename fundraising to donations and add content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,38 @@ import { HomeComponent } from './pages/home/home.component';
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { 
-    path: 'fundraising', 
-    loadComponent: () => import('./pages/fundraising/fundraising.component').then(m => m.FundraisingComponent)
+  {
+    path: 'donations',
+    loadComponent: () =>
+      import('./pages/donations/donations.component').then(
+        (m) => m.DonationsComponent
+      ),
   },
-  { path: 'who-we-are', loadComponent: () => import('./pages/who-we-are/who-we-are.component').then(m => m.WhoWeAreComponent) },
-  { path: 'volunteers', loadComponent: () => import('./pages/volunteers/volunteers.component').then(m => m.VolunteersComponent) },
-  { path: 'register', loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent) },
-  { path: 'faqs', loadComponent: () => import('./pages/faqs/faqs.component').then(m => m.FaqsComponent) },
-  { path: '**', redirectTo: '/home' }
+  {
+    path: 'who-we-are',
+    loadComponent: () =>
+      import('./pages/who-we-are/who-we-are.component').then(
+        (m) => m.WhoWeAreComponent
+      ),
+  },
+  {
+    path: 'volunteers',
+    loadComponent: () =>
+      import('./pages/volunteers/volunteers.component').then(
+        (m) => m.VolunteersComponent
+      ),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
+  },
+  {
+    path: 'faqs',
+    loadComponent: () =>
+      import('./pages/faqs/faqs.component').then((m) => m.FaqsComponent),
+  },
+  { path: '**', redirectTo: '/home' },
 ];
