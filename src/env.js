a9b4e49baa7bd05f5c556afa08b772e0fb96aa21
Adeeb Zaman <zamanadeeb789@gmail.com>
2025-03-22 15:41:15 -0400

    Create Admin view to manage shifts

---
+++ src/env.js
@@ -0,0 +1,7 @@
+// This file will be replaced with actual environment variables during deployment
+
+(function(window) {
+  window.env = window.env || {};
+  // Environment variables
+  window.env.ADMIN_PASSWORD = 'admin123'; // Default for development
+})(this);
